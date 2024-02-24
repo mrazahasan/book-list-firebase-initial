fcfb8c4914bee180f2b8e8322800fa476799b3e0
mrazahasan <m_raza_hassan@hotmail.com>
2024-02-24 17:18:48 +0500

    first commit

---
+++ src/firsbase/config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBvZgUIyP88NfDo0YJ4NydxobFWOgdA-Rc",
+  authDomain: "book-list-with-firebase-a2e87.firebaseapp.com",
+  projectId: "book-list-with-firebase-a2e87",
+  storageBucket: "book-list-with-firebase-a2e87.appspot.com",
+  messagingSenderId: "535835895773",
+  appId: "1:535835895773:web:d9470f87419be713a2f834"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
---
+++ src/views/LoginPage.jsx
@@ -0,0 +1,89 @@
+import FullPageLoader from '../components/FullPageLoader.jsx';
+import { useState } from 'react';
+import { auth } from "../firsbase/config.js";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+function LoginPage() {
+	const [isLoading, setIsLoading] = useState(false);
+	const [loginType, setLoginType] = useState('login');
+	const [userCredentials, setUserCredentials] = useState({});
+	const [error, setError] = useState('');
+
+	function handleCredentials(e) {
+		
+		setUserCredentials({ ...userCredentials, [e.target.name]: e.target.value });
+		console.log(userCredentials);
+	}
+
+	function handleSignup(e) {
+		e.preventDefault();
+		setError("");
+		createUserWithEmailAndPassword(auth, userCredentials.email, userCredentials.password)
+			.then((userCredential) => {
+				// Signed up 
+				const user = userCredential.user;
+
+			})
+			.catch((error) => {
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				setError(error.message);
+			});
+	}
+	function handleLogin(e) {
+		e.preventDefault();
+		console.log("login");
+	}
+
+	return (
+		<>
+			{isLoading && <FullPageLoader></FullPageLoader>}
+
+			<div className="container login-page">
+				<section>
+					<h1>Welcome to the Book App</h1>
+					<p>Login or create an account to continue</p>
+					<div className="login-type">
+						<button
+							className={`btn ${loginType == 'login' ? 'selected' : ''}`}
+							onClick={() => setLoginType('login')}>
+							Login
+						</button>
+						<button
+							className={`btn ${loginType == 'signup' ? 'selected' : ''}`}
+							onClick={() => setLoginType('signup')}>
+							Signup
+						</button>
+					</div>
+					<form className="add-form login">
+						<div className="form-control">
+							<label>Email *</label>
+							<input onChange={(e) => handleCredentials(e)} type="text" name="email" placeholder="Enter your email" />
+						</div>
+						<div className="form-control">
+							<label>Password *</label>
+							<input onChange={(e) => handleCredentials(e)} type="password" name="password" placeholder="Enter your password" />
+						</div>
+						{
+							loginType == 'login' ?
+								<button onClick={(e) => handleLogin(e)} className="active btn btn-block">Login</button>
+								:
+								<button onClick={(e) => handleSignup(e)} className="active btn btn-block">Sign Up</button>
+						}
+						{
+							error && 
+							<div className='error'>
+								{error}
+							</div>
+						}
+
+						<p className="forgot-password">Forgot Password?</p>
+
+					</form>
+				</section>
+			</div>
+		</>
+	)
+}
+
+export default LoginPage
