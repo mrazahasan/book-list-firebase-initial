ea5d79d39482f7a7b046cd6bc86a72090943fc2e
mrazahasan <m_raza_hassan@hotmail.com>
2024-03-04 00:19:40 +0500

    update records

--- src/store/booksSlice.js
+++ src/store/booksSlice.js
@@ -1,66 +1,83 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { collection, doc, getDocs, query, updateDoc, where } from 'firebase/firestore';
+import { db, auth } from '../firsbase/config';
 
 export const booksSlice = createSlice({
-  name: 'books',
-  initialState: [
-    {
-      id: 1,
-      title: "A Short History of Europe",
-      cover:
-        "https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-6-540x861.jpg",
-      isRead: true,
-      author: "Simon Jenkins",
-      synopsis: "In this dazzling new history, bestselling author Simon Jenkins grippingly tells the story of its evolution from warring peoples to peace, wealth and freedom - a story that twists and turns from Greece and Rome, through the Dark Ages, the Reformation and the French Revolution, to the Second World War and up to the present day."
-    },
-    {
-      id: 2,
-      title: "Penguin Classics",
-      cover:
-        "https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-2-540x861.jpg",
-      isRead: false,
-      author: "Henry Eliot",
-      synopsis: "The Penguin Classics Book covers all the greatest works of fiction, poetry, drama, history, and philosophy in between, this reader's companion encompasses 500 authors, 1,200 books, and 4,000 years of world literature, from ancient Mesopotamia to World War I."
-    },
-    {
-      id: 3,
-      title: "Becoming",
-      cover:
-        "https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-7-540x861.jpg",
-      isRead: false,
-      author: "Michelle Obama",
-      synopsis: "“Becoming” is an autobiography detailing the highs and lows of Michelle Obama's incredible journey from humble beginnings in the less glamourous South Side of Chicago, to the grandeur of the White House and life as America's first African-American First Lady."
-    },
-    {
-      id: 4,
-      title: "Sonnets",
-      cover:
-        "https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-5-540x861.jpg",
-      isRead: false,
-      author: "James Anthony",
-      synopsis: "Shakespeare wrote 154 sonnets published in his 'quarto' in 1609, covering themes such as the passage of time, mortality, love, beauty, infidelity, and jealousy. The first 126 of Shakespeare's sonnets are addressed to a young man, and the last 28 addressed to a woman – a mysterious 'dark lady'."
-    }
-  ],
-  reducers: {
-    addBook: (books, action) => {
-      let newBook = action.payload;
-      newBook.id = books.length ? Math.max(...books.map(book => book.id)) + 1 : 1;
-      books.push(newBook);
-    },
-    eraseBook: (books, action) => {
-        return books.filter(book => book.id != action.payload);
-    },
-    toggleRead: (books, action) => {
-        books.map(book => {
-          if (book.id == action.payload) {
-            book.isRead = !book.isRead;
-          }
-        });
-    }
-  }
+	name: 'books',
+	initialState: {
+		books: [],
+		status: "idle"
+	},
+	reducers: {
+		addBook: (books, action) => {
+			let newBook = action.payload;
+			newBook.id = books.length ? Math.max(...books.map(book => book.id)) + 1 : 1;
+			books.push(newBook);
+		},
+		eraseBook: (books, action) => {
+			return books.filter(book => book.id != action.payload);
+		},
+		// toggleRead: (books, action) => {
+		// 	books.map(book => {
+		// 		if (book.id == action.payload) {
+		// 			book.isRead = !book.isRead;
+		// 		}
+		// 	});
+		// }
+	},
+	extraReducers(builder) {
+		builder
+			.addCase(fetchBooks.pending, (state, action) => {
+				state.status = 'loading';
+			})
+			.addCase(fetchBooks.fulfilled, (state, action) => {
+				state.status = 'succeeded';
+				// Add any fetched posts to the array
+				state.books = action.payload;
+			})
+			.addCase(fetchBooks.rejected, (state, action) => {
+				state.status = 'failed';
+				console.log(action.error.message);
+			})
+			.addCase(toggleRead.fulfilled, (state, action) => {
+				state.books.map(book => {
+					if (book.id == action.payload) {
+						book.isRead = !book.isRead;
+					}
+				});
+			})
+			.addCase(toggleRead.rejected, (state, action) => {
+				state.status = 'failed';
+				console.log(action.error.message);
+			})
+	}
 })
 
-export const { addBook, eraseBook, toggleRead } = booksSlice.actions;
+export const { addBook, eraseBook } = booksSlice.actions;
 
 export const selectBooks = state => state.books;
 
 export default booksSlice.reducer;
+
+export const fetchBooks = createAsyncThunk("books/fetchBooks", async () => {
+	const q = query(collection(db, "books"), where("user_id", "==", auth.currentUser.uid));
+	//const q = query(collection(db, "books"));
+
+	const querySnapshot = await getDocs(q);
+	let bookList = [];
+	querySnapshot.forEach((doc) => {
+		// doc.data() is never undefined for query doc snapshots
+		bookList.push({ id: doc.id, ...doc.data() });
+	});
+	//console.log(bookList);
+	return bookList;
+});
+
+export const toggleRead = createAsyncThunk("books/toggleRead", async (payload) => {
+	const bookRef = doc(db, "books", payload.id);
+
+	await updateDoc(bookRef, {
+		isRead: !payload.isRead
+	});
+	return payload.id;
+});
--- src/views/BooksPage.jsx
+++ src/views/BooksPage.jsx
@@ -1,55 +1,43 @@
 import Book from '../components/Book.jsx';
 import Header from '../components/Header.jsx';
-import { useSelector } from 'react-redux';
-import { selectBooks } from '../store/booksSlice.js';
-import { collection, query, where, getDocs } from "firebase/firestore";
-import { db } from '../firsbase/config.js';
-import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchBooks, selectBooks } from '../store/booksSlice.js';
 
 
 function BooksPage() {
 
-  //const books = useSelector(selectBooks);
-  const [books, setBooks] = useState([]);
-  const pageTitle = "📖 Book List with Router, Redux & Firebase";
-  useEffect(() => {
-    const fetchBooks = async () => {
-      //const q = query(collection(db, "books"), where("capital", "==", true));
-      const q = query(collection(db, "books"));
-
-      const querySnapshot = await getDocs(q);
-      let bookList = [];
-      querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        bookList.push({id: doc.id, ...doc.data()});
-      });
-      console.log(bookList);
-      setBooks(bookList);
-    }
-    fetchBooks();
-  }, []);
+	const pageTitle = "📖 Book List with Router, Redux & Firebase";
+	const books = useSelector(selectBooks).books;
+	const booksStatus = useSelector(selectBooks).status;
+	const dispatch = useDispatch();
+	useEffect(() => {
+		if (booksStatus == 'idle') {
+			dispatch(fetchBooks());
+		}
+	}, []);
 
 
 
 
-  return (
-    <>
-      <div className="container">
-        <Header pageTitle={pageTitle} />
-        <div className="books-container">
-          <div className="books-list">
+	return (
+		<>
+			<div className="container">
+				<Header pageTitle={pageTitle} />
+				<div className="books-container">
+					<div className="books-list">
 
-            {books.map(book =>
+						{books.map(book =>
 
-              <Book key={book.id} book={book} />
+							<Book key={book.id} book={book} />
 
-            )}
+						)}
 
-          </div>
-        </div>
-      </div>
-    </>
-  )
+					</div>
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default BooksPage
