a079c99a3d0aab5af1aa2c7df0f827ba054cff2f
mrazahasan <m_raza_hassan@hotmail.com>
2024-03-19 17:01:00 +0000

    video 84 done

--- src/views/SingleBookPage.jsx
+++ src/views/SingleBookPage.jsx
@@ -1,79 +1,111 @@
 import { useParams, Link, useNavigate } from 'react-router-dom';
 import Notes from '../components/Notes.jsx';
-import { useSelector, useDispatch } from 'react-redux';
-import { selectBooks, eraseBook, toggleRead } from '../store/booksSlice.js';
+import { useDispatch } from 'react-redux';
+import {  eraseBook, toggleRead } from '../store/booksSlice.js';
 import { eraseBookNotes } from '../store/notesSlice.js';
+import { useEffect, useState } from 'react';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../firsbase/config.js';
 
 function SingleBookPage() {
 
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
 
-  function handleEraseBook(id) {
-    if (confirm('Are you sure you want to erase this book and all notes associated with it?')) {
-      dispatch(eraseBook(id));
-      //dispatch(eraseBookNotes(id));
-      navigate("/");
-    }
-  }
+	function handleEraseBook(id) {
+		if (confirm('Are you sure you want to erase this book and all notes associated with it?')) {
+			dispatch(eraseBook(id));
+			//dispatch(eraseBookNotes(id));
+			navigate("/");
+		}
+	}
 
-  const { id } = useParams();
+	function handleIsRead(info) {
+		dispatch(toggleRead({ id: info.id, isRead: info.isRead }));
+		setBook({ ...book, isRead: !info.isRead });
+	}
 
-  const books = useSelector(selectBooks).books;
+	const { id } = useParams();
 
-  const book = books.filter(book => book.id == id)[0];
+	// const books = useSelector(selectBooks).books;
 
-  return (
-    <>
-      <div className="container">
-        <Link to="/">
-          <button className="btn">
-            ← Back to Books
-          </button>
-        </Link>
+	// const book = books.filter(book => book.id == id)[0];
+	const [book, setBook] = useState("");
+	const [fetchStatus, setFetchStatus] = useState("idle");
 
-        {book ?
+	useEffect(() => {
+		if (fetchStatus == 'idle') {
+			fetchBooks(id);
+		}
+	}, []);
 
-          <div>
-            <div className="single-book">
-              <div className="book-cover">
-                <img src={book.cover} />
-              </div>
+	const fetchBooks = async (bookId) => {
+		try {
+			const docRef = doc(db, "books", bookId);
+			const docSnap = await getDoc(docRef);
 
-              <div className="book-details">
-                <h3 className="book-title">{book.title}</h3>
-                <h4 className="book-author">{book.author}</h4>
-                <p>{book.synopsis}</p>
-                <div className="read-checkbox">
-                  <input
-                    onClick={() => { dispatch(toggleRead({ id: book.id, isRead: book.isRead })) }}
-                    type="checkbox"
-                    defaultChecked={book.isRead} />
-                  <label>{book.isRead ? "Already Read It" : "Haven't Read it yet"}</label>
-                </div>
-                <div onClick={() => handleEraseBook(book.id)} className="erase-book">
-                  Erase book
-                </div>
-              </div>
-            </div>
+			if (docSnap.exists()) {
+				setBook({ ...docSnap.data(), id: docSnap.id });
+			}
+			setFetchStatus("success");
+		}
+		catch (err) {
+			console.log("error", err);
+			setFetchStatus("error");
+		}
+	}
 
-            <Notes bookId={id} />
-          </div>
+	return (
+		<>
+			<div className="container">
+				<Link to="/">
+					<button className="btn">
+						← Back to Books
+					</button>
+				</Link>
 
-          :
+				{book ?
 
-          <div>
-            <p>Book not found. Click the button above to go back to the list of books.</p>
-          </div>
+					<div>
+						<div className="single-book">
+							<div className="book-cover">
+								<img src={book.cover} />
+							</div>
 
-        }
+							<div className="book-details">
+								<h3 className="book-title">{book.title}</h3>
+								<h4 className="book-author">{book.author}</h4>
+								<p>{book.synopsis}</p>
+								<div className="read-checkbox">
+									<input
+										onClick={() => { handleIsRead({ id: book.id, isRead: book.isRead }) }}
+										type="checkbox"
+										defaultChecked={book.isRead} />
+									<label>{book.isRead ? "Already Read It" : "Haven't Read it yet"}</label>
+								</div>
+								<div onClick={() => handleEraseBook(book.id)} className="erase-book">
+									Erase book
+								</div>
+							</div>
+						</div>
+
+						<Notes bookId={id} />
+					</div>
+
+					:
+
+					<div>
+						<p>Book not found. Click the button above to go back to the list of books.</p>
+					</div>
+
+				}
 
 
-      </div>
+			</div>
 
 
-    </>
-  )
+		</>
+	)
 }
 
 export default SingleBookPage
